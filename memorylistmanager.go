2fa09354f5ac339e13a0d5400ee4645f553b73b7
Harry Jeffery <harry@exec64.co.uk>
2018-05-16 21:09:52 +0100

    Add MemoryListManager

---
+++ memorylistmanager.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+)
+
+type MemoryListManager struct {
+	lists map[string]*List
+}
+
+func (m *MemoryListManager) init() {
+	m.lists = make(map[string]*List)
+}
+
+func (m *MemoryListManager) Add(list *List) error {
+	if m.lists == nil {
+		m.init()
+	}
+
+	m.lists[list.Id] = list
+	return nil
+}
+
+func (m *MemoryListManager) Remove(id string) error {
+	if m.lists == nil {
+		m.init()
+	}
+
+	delete(m.lists, id)
+	return nil
+}
+
+func (m *MemoryListManager) FetchListIds() ([]string, error) {
+	if m.lists == nil {
+		m.init()
+	}
+
+	ids := []string{}
+
+	for id, _ := range m.lists {
+		ids = append(ids, id)
+	}
+
+	return ids, nil
+}
+
+func (m *MemoryListManager) FetchList(id string) (*List, error) {
+	if m.lists == nil {
+		m.init()
+	}
+
+	list, ok := m.lists[id]
+	if !ok {
+		return nil, errors.New("Invalid list")
+	}
+
+	return list, nil
+}
