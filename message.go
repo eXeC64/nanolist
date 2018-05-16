55bc2d8ca4d68ba5b75c97bcb3bfb79e6ef8e04d
Harry Jeffery <harry@exec64.co.uk>
2018-05-16 21:26:17 +0100

    Add ParseMessage

--- message.go
+++ message.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"io"
+	"io/ioutil"
+	"net/mail"
+)
+
 type Message struct {
 	Subject     string
 	From        string
@@ -12,4 +18,31 @@ type Message struct {
 	ContentType string
 	XList       string
 	Body        string
+}
+
+func ParseMessage(input io.Reader) (*Message, error) {
+
+	inMessage, err := mail.ReadMessage(input)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(inMessage.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := &Message{
+		Subject:   inMessage.Header.Get("Subject"),
+		From:      inMessage.Header.Get("From"),
+		Id:        inMessage.Header.Get("Message-ID"),
+		InReplyTo: inMessage.Header.Get("In-Reply-To"),
+		Body:      string(body[:]),
+		To:        inMessage.Header.Get("To"),
+		Cc:        inMessage.Header.Get("Cc"),
+		Bcc:       inMessage.Header.Get("Bcc"),
+		Date:      inMessage.Header.Get("Date"),
+	}
+
+	return msg, nil
 }
