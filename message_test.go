cc60bf6e704e03ab67957d9dc759dc349d94adeb
Harry Jeffery <harry@exec64.co.uk>
2018-05-29 21:04:16 +0100

    Message: Refactor testing

--- message_test.go
+++ message_test.go
@@ -7,6 +7,82 @@ import (
 	"time"
 )
 
+func checkMessage(expected *Message, actual *Message, t *testing.T) {
+
+	if expected.From != nil {
+		if expected.From.Name != actual.From.Name {
+			t.Errorf("Expected from name (%q) != actual from name %q", expected.From.Name, actual.From.Name)
+		}
+
+		if expected.From.Address != actual.From.Address {
+			t.Errorf("Expected address (%q) != actual address %q", expected.From.Address, actual.From.Address)
+		}
+	}
+
+	if len(expected.To) != len(actual.To) {
+		t.Errorf("Expected To length (%d) != Actual To length (%d)", len(expected.To), len(actual.To))
+	} else {
+		for i := range expected.To {
+			if expected.To[i].Name != actual.To[i].Name {
+				t.Errorf("Expected To[%d] name (%q) != Actual To[%d] name (%q)", i, expected.To[i].Name, i, actual.To[i].Name)
+			}
+			if expected.To[i].Address != actual.To[i].Address {
+				t.Errorf("Expected To[%d] address (%q) != Actual To[%d] address (%q)", i, expected.To[i].Address, i, actual.To[i].Address)
+			}
+		}
+	}
+
+	if len(expected.Cc) != len(actual.Cc) {
+		t.Errorf("Expected Cc length (%d) != Actual Cc length (%d)", len(expected.Cc), len(actual.Cc))
+	} else {
+		for i := range expected.Cc {
+			if expected.Cc[i].Name != actual.Cc[i].Name {
+				t.Errorf("Expected Cc[%d] name (%q) != Actual Cc[%d] name (%q)", i, expected.Cc[i].Name, i, actual.Cc[i].Name)
+			}
+			if expected.Cc[i].Address != actual.Cc[i].Address {
+				t.Errorf("Expected Cc[%d] address (%q) != Actual Cc[%d] address (%q)", i, expected.Cc[i].Address, i, actual.Cc[i].Address)
+			}
+		}
+	}
+
+	if len(expected.Bcc) != len(actual.Bcc) {
+		t.Errorf("Expected Bcc length (%d) != Actual Bcc length (%d)", len(expected.Bcc), len(actual.Bcc))
+	} else {
+		for i := range expected.Bcc {
+			if expected.Bcc[i].Name != actual.Bcc[i].Name {
+				t.Errorf("Expected Bcc[%d] name (%q) != Actual Bcc[%d] name (%q)", i, expected.Bcc[i].Name, i, actual.Bcc[i].Name)
+			}
+			if expected.Bcc[i].Address != actual.Bcc[i].Address {
+				t.Errorf("Expected Bcc[%d] address (%q) != Actual Bcc[%d] address (%q)", i, expected.Bcc[i].Address, i, actual.Bcc[i].Address)
+			}
+		}
+	}
+
+	if expected.Subject != actual.Subject {
+		t.Errorf("Expected Subject (%q) != Actual Subject (%q)", expected.Subject, actual.Subject)
+	}
+
+	if expected.Date.String() != actual.Date.String() {
+		t.Errorf("Expected Date (%q) != Actual Date (%q)", expected.Date.String(), actual.Date.String())
+	}
+
+	if expected.Id != actual.Id {
+		t.Errorf("Expected Id (%q) != Actual Id (%q)", expected.Id, actual.Id)
+	}
+
+	if expected.InReplyTo != actual.InReplyTo {
+		t.Errorf("Expected InReplyTo (%q) != Actual InReplyTo (%q)", expected.InReplyTo, actual.InReplyTo)
+	}
+
+	if expected.ContentType != actual.ContentType {
+		t.Errorf("Expected ContentType (%q) != Actual ContentType (%q)", expected.ContentType, actual.ContentType)
+	}
+
+	if expected.Body != actual.Body {
+		t.Errorf("Expected Body (%q) != Actual Body (%q)", expected.Body, actual.Body)
+	}
+}
+
 func TestParseMessage(t *testing.T) {
 	fakeInput := strings.NewReader("" +
 		"From: \"Example Person\" <test@example.com>\r\n" +
@@ -21,102 +97,31 @@ func TestParseMessage(t *testing.T) {
 		"\r\n" +
 		"This is my body\nIt even has multiple lines.")
 
-	msg, err := ParseMessage(fakeInput)
+	actual, err := ParseMessage(fakeInput)
 	if err != nil {
 		t.Errorf("Parsing failed with error: %s", err.Error())
 		return
 	}
 
-	if msg == nil {
+	if actual == nil {
 		t.Errorf("Returned message was nil")
 		return
 	}
 
-	if msg.From.Name != "Example Person" {
-		t.Errorf("From name parsed incorrectly")
+	expected := &Message{
+		Subject:     "My Test Subject",
+		From:        &mail.Address{Name: "Example Person", Address: "test@example.com"},
+		Id:          "test-msg-id@example.com",
+		InReplyTo:   "other-msg-id@example.com",
+		ContentType: "text/plain",
+		Body:        "This is my body\nIt even has multiple lines.",
+		To:          []*mail.Address{{"Alice", "alice@example.com"}, {"Bob", "bob@example.com"}},
+		Cc:          []*mail.Address{{"Charlie", "charlie@example.com"}, {"Dolores", "dolores@example.com"}},
+		Bcc:         []*mail.Address{{"Evan", "evan@example.com"}, {"Francis", "francis@example.com"}},
+		Date:        time.Date(2018, time.May, 14, 19, 41, 34, 0, time.FixedZone("+0200", 2*60*60)),
 	}
 
-	if msg.From.Address != "test@example.com" {
-		t.Errorf("From address parsed incorrectly")
-	}
-
-	if len(msg.To) != 2 {
-		t.Errorf("To parsed with incorrect number of entries")
-	} else {
-		if msg.To[0].Name != "Alice" {
-			t.Errorf("To first name parsed incorrectly")
-		}
-		if msg.To[0].Address != "alice@example.com" {
-			t.Errorf("To first address parsed incorrectly")
-		}
-
-		if msg.To[1].Name != "Bob" {
-			t.Errorf("To second name parsed incorrectly")
-		}
-		if msg.To[1].Address != "bob@example.com" {
-			t.Errorf("To second address parsed incorrectly")
-		}
-	}
-
-	if len(msg.Cc) != 2 {
-		t.Errorf("Cc parsed with incorrect number of entries")
-	} else {
-		if msg.Cc[0].Name != "Charlie" {
-			t.Errorf("Cc first name parsed incorrectly")
-		}
-		if msg.Cc[0].Address != "charlie@example.com" {
-			t.Errorf("Cc first address parsed incorrectly")
-		}
-
-		if msg.Cc[1].Name != "Dolores" {
-			t.Errorf("Cc second name parsed incorrectly")
-		}
-		if msg.Cc[1].Address != "dolores@example.com" {
-			t.Errorf("Cc second address parsed incorrectly")
-		}
-	}
-
-	if len(msg.Bcc) != 2 {
-		t.Errorf("Bcc parsed with incorrect number of entries")
-	} else {
-		if msg.Bcc[0].Name != "Evan" {
-			t.Errorf("Bcc first name parsed incorrectly")
-		}
-		if msg.Bcc[0].Address != "evan@example.com" {
-			t.Errorf("Bcc first address parsed incorrectly")
-		}
-
-		if msg.Bcc[1].Name != "Francis" {
-			t.Errorf("Bcc second name parsed incorrectly")
-		}
-		if msg.Bcc[1].Address != "francis@example.com" {
-			t.Errorf("Bcc second address parsed incorrectly")
-		}
-	}
-
-	if msg.Subject != "My Test Subject" {
-		t.Errorf("Subject parsed incorrectly")
-	}
-
-	if msg.Date.String() != "2018-05-14 19:41:34 +0200 +0200" {
-		t.Errorf("Date parsed incorrectly: %s", msg.Date.String())
-	}
-
-	if msg.Id != "test-msg-id@example.com" {
-		t.Errorf("Message id parsed incorrectly")
-	}
-
-	if msg.InReplyTo != "other-msg-id@example.com" {
-		t.Errorf("In-Reply-To parsed incorrectly")
-	}
-
-	if msg.ContentType != "text/plain" {
-		t.Errorf("Content-Type parsed incorrectly")
-	}
-
-	if msg.Body != "This is my body\nIt even has multiple lines." {
-		t.Error("Body parsed incorrectly")
-	}
+	checkMessage(expected, actual, t)
 }
 
 func TestParseSlimMessage(t *testing.T) {
@@ -126,39 +131,24 @@ func TestParseSlimMessage(t *testing.T) {
 		"Subject: My Test Subject\r\n" +
 		"\r\n")
 
-	msg, err := ParseMessage(fakeInput)
+	actual, err := ParseMessage(fakeInput)
 	if err != nil {
 		t.Errorf("Parsing failed with error: %s", err.Error())
 		return
 	}
 
-	if msg == nil {
+	if actual == nil {
 		t.Errorf("Returned message was nil")
 		return
 	}
 
-	if msg.From.Name != "Example Person" {
-		t.Error("From name parsed incorrectly")
+	expected := &Message{
+		Subject: "My Test Subject",
+		From:    &mail.Address{Name: "Example Person", Address: "test@example.com"},
+		To:      []*mail.Address{{"Alice", "alice@example.com"}},
 	}
 
-	if msg.From.Address != "test@example.com" {
-		t.Error("From name parsed incorrectly")
-	}
-
-	if msg.Subject != "My Test Subject" {
-		t.Error("Subject parsed incorrectly")
-	}
-
-	if len(msg.To) != 1 {
-		t.Error("To parsed incorrect number of addresses")
-	} else {
-		if msg.To[0].Name != "Alice" {
-			t.Error("To name parsed incorrectly")
-		}
-		if msg.To[0].Address != "alice@example.com" {
-			t.Error("To address parsed incorrectly")
-		}
-	}
+	checkMessage(expected, actual, t)
 }
 
 func TestStringMessage(t *testing.T) {
@@ -201,28 +191,19 @@ func TestReply(t *testing.T) {
 		Id:      "test-id@example.com",
 	}
 
-	reply := msg.Reply()
-
-	if reply.Subject != "Re: This is a test" {
-		t.Errorf("Incorrect Subject: %q", reply.Subject)
+	expected := &Message{
+		Subject:   "Re: This is a test",
+		To:        []*mail.Address{{"James Bond", "bond@example.com"}},
+		InReplyTo: "test-id@example.com",
+		Date:      time.Now(),
 	}
 
-	if len(reply.To) != 1 {
-		t.Errorf("Incorrect number of recipients: %d", len(reply.To))
-	} else {
+	actual := msg.Reply()
 
-		if reply.To[0].Name != "James Bond" {
-			t.Errorf("Incorrect To name: %q", reply.To[0].Name)
-		}
+	// Cheat this one field, because time passes
+	actual.Date = expected.Date
 
-		if reply.To[0].Address != "bond@example.com" {
-			t.Errorf("Incorrect To address: %q", reply.To[0].Address)
-		}
-	}
-
-	if reply.InReplyTo != "test-id@example.com" {
-		t.Errorf("Incorrect InReplyTo: %q", reply.InReplyTo)
-	}
+	checkMessage(expected, actual, t)
 }
 
 func TestAllRecipients(t *testing.T) {
